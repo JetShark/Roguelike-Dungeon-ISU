bbf7247e9f6fbbfbc3380bdc896ae238c1a07581
JetShark <JetShark02@gmail.com>
2020-05-06 00:21:01 -0400

    Map Displays!
    Switched map file type from .tmx to .csv, removed Tiled java library

--- Michelangelo/src/Cowabunga.java
+++ Michelangelo/src/Cowabunga.java
@@ -1,44 +1,17 @@
-import java.awt.*;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import org.mapeditor.*;
+
 public class Cowabunga extends JPanel {
     //@Override
-    /*
-    private Building b1 = (new Building(10, 400, 80, 200));
-    //private Building b2 = (new Building(200, 400, 80, 200));
-    //private Building b2 = (new Building(200, 300, 160, 400));
-    //private Building b3 = (new Building(450, 400, 120, 300));
-    //private Building b4 = (new Building(720, 400, 80, 200));
-    //private Building b5 = (new Building(850, 350, 120, 250));
-    private Car car = (new Car(this));
-    private Building b2 = (new Building(200, 300, 160, 300));
-    private Building b3 = (new Building(450, 400, 120, 200));
-    private Building b4 = (new Building(720, 400, 80, 200));
-    private Building b5 = (new Building(850, 350, 120, 250));
 
-    //private UFO b = new UFO(this); //Add an instance of the ball object
-    //private UFO c = new UFO(this, 90, 90, 5, 5); //Add an instance of the ball object
+    MapLayer currentMap;
 
-    static UFO[] list = new UFO[5];
-    */
+    public Cowabunga() throws Exception {
 
-    public Cowabunga() {
-        //Randomly create each Ball in the list
-        for (int i = 0; i< list.length; i++) {
-            if (i==0) {
-                list[i] = new UFO(this, 400, 40, 0, 0, true);;
-            } else {
-                list[i] = new UFO(this, i*55, i*55,
-                        ((int)(10 * Math.random() - 5)+1),
-                        ((int)(10 * Math.random() - 5)+1),
-                        false);
+        currentMap = new MapLayer("Map System/Test Map_Map.csv", "Map System/DawnLike/Objects/floor.png");
 
-            }
-        }
 
 
         //This adds the KeyListener to the BallWorld. It’s in this section that
@@ -55,12 +28,12 @@ public class Cowabunga extends JPanel {
             @Override
             public void keyReleased(KeyEvent e) {
                 //Passes the KeyEvent e to the ball instance
-                list[0].keyReleased(e);
+                //list[0].keyReleased(e);
             }
             @Override
             public void keyPressed(KeyEvent e) {
                 //Passes the KeyEvent e to the ball instance
-                list[0].keyPressed(e);
+                //list[0].keyPressed(e);
             }
         }); //NOTE THE SEMI-COLON!!!!
 
@@ -71,22 +44,7 @@ public class Cowabunga extends JPanel {
 
 
     private void move() { //Have the ball move
-        //b.move();
-        //c.move();
-
-        //Check for collisions between each of the balls
-        for (int i = 0; i < list.length; i++)
-            for (int j = i+1; j<list.length; j++)
-                list[i].collision(list[j]);
-        //Move each Ball in the list
-        for (UFO b:list) {
-            b.move();
-        }
-
-        if (car.beamed != true) {
-            car.move();
-            car.tractor();
-        }
+        // FIXME: 2020-05-05 need to have the player move function
     }
 
     public void paint(Graphics g) {
@@ -95,36 +53,18 @@ public class Cowabunga extends JPanel {
         g2d.setColor(Color.RED);
         g2d.setBackground(Color.black);
 
-        b1.paint(g2d);
-        b2.paint(g2d);
-        b3.paint(g2d);
-        b4.paint(g2d);
-        b5.paint(g2d);
-
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        //b.paint(g2d); //Have the ball draw itself
-        //c.paint(g2d);
-        /*
-        for (UFO b:list) {
-            b.paint(g2d);
-        }
-        if (car.beamed != true) {
-            car.paint(g2d);
-        }
-        */
+        currentMap.paint(g2d);
 
     }
-    public static void main(String[] args) throws InterruptedException {
+
+    public static void main(String[] args) throws Exception {
         JFrame frame = new JFrame("Michelangelo");
         //Add our JPanel to the frame
-        frame.add(new Cowabunga());
+        Cowabunga p = new Cowabunga();
+        frame.add(p);
         frame.setSize(1020 ,640);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Cowabunga p = new Cowabunga();
-        frame.add(p);
 
         while (true)
         {
---
+++ Michelangelo/src/MapLayer.java
@@ -0,0 +1,78 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class MapLayer {
+    //Attributes
+
+    private Cowabunga ba;
+    private int width = 0;
+    private int height = 0;
+    private int tilesAcross = 0;
+    private int tilesDown = 0;
+    private static final int tileWidth = 16;
+    private static final int tileHeight = 16;
+    private BufferedImage img = null;
+    private int[][] level;
+
+
+
+//Constructor
+
+    public MapLayer(String mapPath, String tilePath) {
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader(mapPath));
+            try {
+                String row;
+
+                while ((row = csvReader.readLine()) != null) {
+                    String[] data = row.split(",");
+                    height++;
+                    if (data.length > width) {
+                        width = data.length;
+                    }
+                }
+                csvReader.close();
+                level = new int[width][height];
+                csvReader = new BufferedReader(new FileReader(mapPath));
+                int y = 0;
+                while ((row = csvReader.readLine()) != null) {
+                    String[] data = row.split(",");
+                    for (int x = 0; x < data.length; x++){
+                        level[x][y]= Integer.parseInt(data[x]);
+                    }
+                    y++;
+                }
+                System.out.println("Map Width & Height = " + width + ", " + height);
+                img = ImageIO.read(new File(tilePath));
+                tilesAcross = img.getWidth() / tileWidth;
+                tilesDown = img.getHeight() / tileHeight;
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println(e.toString());
+            }
+            this.ba = ba;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+    }
+//Accessors
+    public int getLevel(int x, int y) {
+        return level[x][y];
+    }
+
+//Mutators
+
+//Methods
+    public void paint(Graphics2D g) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int i = (level[x][y])%tilesAcross;
+                int j = (level[x][y])/tilesAcross;
+                g.drawImage(img.getSubimage(i*tileWidth, j*tileHeight, tileWidth,tileHeight), x*tileWidth*2, y*tileHeight*2, tileWidth*2, tileHeight*2, null);
+            }
+        }
+    }
+}
