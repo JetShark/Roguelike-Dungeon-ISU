e84e120ac5d8652cdabefa8c7999bbe7d5b4d687
DeathLemons <64559878+DeathLemons@users.noreply.github.com>
2020-05-24 17:18:23 -0400

    start genration of weapons on player

--- Michelangelo/src/Player.java
+++ Michelangelo/src/Player.java
@@ -9,6 +9,7 @@ public class Player {
     private int x, y, xA, yA;
     private int oldX, oldY;
     private int speedx = 0, speedy = 0;
+    private Weapons w;
     private boolean right = false, left = false;
     private Boolean up = false, down = false;
     private boolean dodgeRoll = false;
@@ -98,6 +99,7 @@ public class Player {
         offsetMinY = 0;
         camX = this.x - VIEWPORT_SIZE_X/2;
         camY = this.y - VIEWPORT_SIZE_Y/2;
+        //w = new Weapons(x,y);
     }
 
     public void keyPressed(KeyEvent e){
@@ -162,6 +164,7 @@ public class Player {
     }
 
     public void move(){
+        //w = new Weapons(x,y);
         animation.update();
         int xt = x, yt = y;
         /*if (right && this.x + 32 < cb.getWidth() - 48){
@@ -237,6 +240,7 @@ public class Player {
         }else if (camY < offsetMinY) {
             camY = offsetMinY;
         }
+        //w.paint(g2d);
         if(direction == 1){
             g2d.drawImage(animation.getSprite(), x, y, animation.getSprite().getHeight() * 2, animation.getSprite().getWidth() * 2 , null);
         }
@@ -256,6 +260,12 @@ public class Player {
     }
     public int getCamY(){
         return this.camY;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
     }
     public void setProjetile(boolean projetile){
         this.projetile = projetile;
