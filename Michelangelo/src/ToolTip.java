7cc97b8c517f2e17e6f39f7c1fc333c9a215f0c9
DeathLemons <64559878+DeathLemons@users.noreply.github.com>
2020-06-03 14:10:03 -0400

    added tooltip (still in progress)
    just added x and y values that detrme where it draws.

--- Michelangelo/src/ToolTip.java
+++ Michelangelo/src/ToolTip.java
@@ -10,21 +10,23 @@ public class ToolTip {
     private ItemsInformation ii;
     public ToolTip(){
         ii = new ItemsInformation("src/ItemInformation/Item-Information.txt", "1");
+        x = 800;
+        y = 1000;
     }
     public void paint(Graphics2D g2d){
         float thickness = 2;
         Stroke oldStroke = g2d.getStroke();
         g2d.setColor(Color.BLACK);
         g2d.setStroke(new BasicStroke(thickness));
-        g2d.drawRect(1500,1000, 200, 100);
+        g2d.drawRect(x,y, 200, 100);
         g2d.setStroke(oldStroke);
         g2d.setColor(new Color(0,0,225,150));
-        g2d.fillRect(1501, 1001, 199, 99);
+        g2d.fillRect(x + 1, y + 1, 199, 99);
         g2d.setColor(Color.WHITE);
-        g2d.drawString(ii.getName(), 1502, 1012);
-        g2d.drawString("Damage: " + ii.getDamage(), 1502, 1024);
-        g2d.drawString("Number of Shots: " + ii.getNumOfShots(), 1502, 1036);
-        g2d.drawString("Reload Time: " + ii.getReloadTime() + " Seconds", 1502, 1048);
-        g2d.drawString("Special Effect: Knocks enemies \nback", 1502, 1060);
+        g2d.drawString(ii.getName(), x + 2, y + 12);
+        g2d.drawString("Damage: " + ii.getDamage(), x + 2, y + 24);
+        g2d.drawString("Number of Shots: " + ii.getNumOfShots(), x + 2, y + 36);
+        g2d.drawString("Reload Time: " + ii.getReloadTime() + " Seconds", x + 2, y + 48);
+        g2d.drawString("Special Effect: Knocks enemies \nback", x + 2, y + 60);
     }
 }
