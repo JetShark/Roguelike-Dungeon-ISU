e7e1132939340e51845326d308b32334ec709a8c
DeathLemons <64559878+DeathLemons@users.noreply.github.com>
2020-06-12 16:44:40 -0400

    added key to sheet, added collection class

---
+++ Michelangelo/src/Collection.java
@@ -0,0 +1,21 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+public class Collection {
+    public Collection(){
+
+    }
+    public void mousePressed(MouseEvent e){
+
+    }
+    public void mouseReleased(MouseEvent e){
+
+    }
+    public void paint(Graphics2D g2d){
+
+    }
+}
