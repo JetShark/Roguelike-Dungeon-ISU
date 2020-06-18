f3f19a91e0cad853a8feefbaf75d4df38776a696
DeathLemons <64559878+DeathLemons@users.noreply.github.com>
2020-06-17 21:35:43 -0400

    bombs blow up dont deal damage yet

--- Michelangelo/src/Bomb.java
+++ Michelangelo/src/Bomb.java
@@ -8,6 +8,8 @@ public class Bomb {
     private int x,y;
     private boolean bombPlaced;
     private BufferedImage img;
+    private int detonation = 150;
+    private boolean blownUp, blowUp;
     public Bomb(Cowabunga cb){
         this.cb = cb;
         bombPlaced = false;
@@ -19,10 +21,19 @@ public class Bomb {
             this.y = p.getY();
             bombPlaced = true;
             wd.minusBombs(1);
+            blowUp = true;
+            //detonation = 100;
         }
     }
+    public void blowUp(){
+        if(detonation == 0){
+            blownUp = true;
+            detonation = 150;
+        }
+        detonation--;
+    }
     public void paint(Graphics2D g2d){
-        if(bombPlaced){
+        if(bombPlaced && !blownUp){
             //System.out.println("x,y: " + x + "," + y);
             g2d.drawImage(img, x,y, null);
         }
@@ -30,5 +41,8 @@ public class Bomb {
     public void move(){
         wd = cb.getWd();
         p = cb.getP();
+        if(blowUp) {
+            blowUp();
+        }
     }
 }
